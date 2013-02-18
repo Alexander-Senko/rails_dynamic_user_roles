c791e16673d73aa3038307a145db912695bf3fb5
Alexander Senko <Alexander.Senko@gmail.com>
2013-02-18 22:06:28 +0700

    Added migration for User::Permit.

---
+++ db/migrate/01_create_user_permits.rb
@@ -0,0 +1,12 @@
+class CreateUserPermits < ActiveRecord::Migration
+	def change
+		create_table :user_permits do |t|
+			t.string     :behavior, null: false
+			t.string     :action,   null: false
+			t.references :subject,  polymorphic: true
+			t.references :role
+
+			t.timestamps
+		end
+	end
+end
