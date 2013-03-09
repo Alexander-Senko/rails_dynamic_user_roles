dd764c4b7a13023aee91a23ab0dfe721e0ee1c4c
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-09 12:03:22 +0700

    Ability: fixed for guests.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,12 @@ class Ability
 	include CanCan::Ability
 
 	def initialize user
-		for permit in user.permits + User::Permit.where(role_id: nil) do
+		for permit in permits_for user do
 			send permit.behavior, permit.action.to_sym, permit.subject || :all
 		end
+	end
+
+	def permits_for user
+		(user.try(:permits) || []) + User::Permit.global
 	end
 end
