c9e36f97bb501b6af41af32188600df49335acbe
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-24 23:28:32 +0700

    User: fixed roles & removed scoping.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :relations, as: :source, conditions: { target_type: nil, target_id: nil }
-	has_many :roles,     through: :relations
-	has_many :permits,   through: :roles
+	has_many :relations, as: :source
+	has_many :roles,   through: :relations, class_name: '::Role'
+	has_many :permits, through: :roles
 end
