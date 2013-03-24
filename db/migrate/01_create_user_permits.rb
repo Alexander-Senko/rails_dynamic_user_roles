d44bcd51371d20e4c3900e755f339aba59a46460
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-24 16:06:51 +0700

    Permits: added default behavior.

--- db/migrate/01_create_user_permits.rb
+++ db/migrate/01_create_user_permits.rb
@@ -1,7 +1,7 @@
 class CreateUserPermits < ActiveRecord::Migration
 	def change
 		create_table :user_permits do |t|
-			t.string     :behavior, null: false
+			t.string     :behavior, null: false, default: :can
 			t.string     :action,   null: false
 			t.references :subject,  polymorphic: true
 			t.references :role
